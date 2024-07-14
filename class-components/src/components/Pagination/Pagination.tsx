0e8e4937f438374d344a9aaf68b44d762240cb3e
Sergey <sk85post@gmail.com>
2024-07-14 19:09:54 +0300

    feat: display the current page in the browser URL

--- class-components/src/components/Pagination/Pagination.tsx
+++ class-components/src/components/Pagination/Pagination.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import './Pagination.css';
 import { IPaginationProps } from '../../types/PaginationTypes';
+import { useNavigate } from 'react-router-dom';
 
 const Pagination: React.FC<IPaginationProps> = ({
   currentPage,
   totalPages,
   onPageChange,
 }) => {
-  const handleFirstPage = () => onPageChange(1);
-  const handleLastPage = () => onPageChange(totalPages);
-  const handlePrevPage = () => onPageChange(currentPage - 1);
-  const handleNextPage = () => onPageChange(currentPage + 1);
+  const navigate = useNavigate();
+
+  const handlePageChange = (page: number) => {
+    onPageChange(page);
+    navigate(`?page=${page}`);
+  };
+  const handleFirstPage = () => handlePageChange(1);
+  const handleLastPage = () => handlePageChange(totalPages);
+  const handlePrevPage = () => handlePageChange(currentPage - 1);
+  const handleNextPage = () => handlePageChange(currentPage + 1);
 
   const renderPageNumbers = () => {
     const pages = [];
@@ -20,7 +27,7 @@ const Pagination: React.FC<IPaginationProps> = ({
           type="button"
           key={i}
           className={`page-button ${i === currentPage ? 'active' : ''}`}
-          onClick={() => onPageChange(i)}
+          onClick={() => handlePageChange(i)}
         >
           {i}
         </button>,
